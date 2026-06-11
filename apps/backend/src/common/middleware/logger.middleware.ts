9e43f309fc286833078ef1686f6b47b560d1d141
Xuccessor <ameeribro4@gmail.com>
2026-06-11 09:42:53 +0000

    fix: replace any types with Express Request/Response/NextFunction in LoggerMiddleware

--- apps/backend/src/common/middleware/logger.middleware.ts
+++ apps/backend/src/common/middleware/logger.middleware.ts
@@ -1,10 +1,11 @@
 import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
   private readonly logger = new Logger('HTTP');
 
-  use(req: any, res: any, next: () => void) {
+  use(req: Request, res: Response, next: NextFunction) {
     const start = Date.now();
     res.on('finish', () => {
       this.logger.log(`${req.method} ${req.path} ${res.statusCode} ${Date.now() - start}ms`);
